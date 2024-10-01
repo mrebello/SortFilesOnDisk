f431bf976818628d260173e04682a780e4f64b1c
Marcos Paulo Ferreira Rebello <marcos.rebello@hotmail.com>
2024-10-01 10:18:42 -0300

    Adicionar arquivos de projeto.

---
+++ SortFAT/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SortFAT {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(5, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(340, 368);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(5, 4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(173, 43);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Use [ctrl] para mover";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Sort by name";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 422);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Reorder files";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SortFAT/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SortFAT {
+        public partial class Form1 : Form {
+        string basedir;
+           
+        public Form1() {
+            InitializeComponent();
+            listBox1.KeyDown += new KeyEventHandler(listBox1_KeyDown);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // preencher listbox com drives do sistema
+            foreach (var drive in System.IO.DriveInfo.GetDrives()) {
+                if (drive.DriveType == System.IO.DriveType.Removable) {
+                    listBox2.Items.Add(drive.Name + " - " + drive.VolumeLabel);
+                }
+            }
+            //seleciona o primeiro item da listbox
+            listBox2.SelectedIndex = 0;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e) {
+            // preencher listbox com todos os arquivos do drive selecionado
+            listBox1.Items.Clear();
+            basedir = listBox2.SelectedItem.ToString().Substring(0, 3);
+
+            foreach (var file in System.IO.Directory.GetFiles(basedir,"*.*",System.IO.SearchOption.AllDirectories)) {
+                if (file.Contains("System Volume Information")) continue;
+                listBox1.Items.Add(file.Substring(3));
+            }
+
+            button2_Click(null, null);
+        }
+
+
+        // permitir reordenar os itens da listbox com as setas de direção
+        private void listBox1_KeyDown(object sender, KeyEventArgs e) {
+            // verifica se ctrl está pressionado
+            if (!e.Control) return;
+
+            if (listBox1.SelectedItem == null) return;
+
+            int selectedIndex = listBox1.SelectedIndex;
+            if (e.KeyCode == Keys.Up && selectedIndex > 0) {
+                // Move item up
+                var item = listBox1.SelectedItem;
+                listBox1.Items.RemoveAt(selectedIndex);
+                listBox1.Items.Insert(selectedIndex - 1, item);
+                listBox1.SelectedIndex = selectedIndex;
+            } else if (e.KeyCode == Keys.Down && selectedIndex < listBox1.Items.Count - 1) {
+                // Move item down
+                var item = listBox1.SelectedItem;
+                listBox1.Items.RemoveAt(selectedIndex);
+                listBox1.Items.Insert(selectedIndex + 1, item);
+                listBox1.SelectedIndex = selectedIndex;
+            }
+        }
+
+               
+
+        private void button1_Click(object sender, EventArgs e) {
+            // para cada item da listbox, definir o nome curto
+            for (int i = 0; i < listBox1.Items.Count; i++) {
+                string fileori = basedir + listBox1.Items[i].ToString();
+
+                string extensao = System.IO.Path.GetExtension(fileori);
+                string path = System.IO.Path.GetFullPath(fileori);
+                string filename = System.IO.Path.GetFileNameWithoutExtension(fileori);
+                filename = filename.TrimStart(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_', ' ' });
+
+                string num = (i + 1).ToString("000");
+
+                string novonome = basedir + num + "- " + filename + extensao;
+
+                //renomar arquivo
+                System.IO.File.Move(fileori, novonome);
+            }
+
+            // mostra aviso de salvo
+            MessageBox.Show("Salvo!");
+
+            listBox2_SelectedIndexChanged(null, null);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            // ordena o listbox pelo nome do item
+            List<string> items = new List<string>();
+            foreach (var item in listBox1.Items) {
+                items.Add(item.ToString());
+            }
+
+            items.Sort();
+
+            listBox1.Items.Clear();
+            foreach (var item in items) {
+                listBox1.Items.Add(item);
+            }
+
+
+        }
+    }
+}
